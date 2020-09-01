bf43109256084faf00c63568a5ff5c30ea09bae9
Sree Revoori <srrevoori@gmail.com>
2020-09-01 07:48:44 -0400

    Update enigma.py

--- enigma.py
+++ enigma.py
@@ -3,3 +3,16 @@ reflector = "UKW-B"
 ringSettings ="ABC"
 ringPositions = "DEF" 
 plugboard = "AT BS DE FM IR KN LZ OW PV XY"
+
+def caesarShift(str, amount):
+	output = ""
+
+	for i in range(0,len(str)):
+		c = str[i]
+		code = ord(c)
+		if ((code >= 65) and (code <= 90)):
+			c = chr(((code - 65 + amount) % 26) + 65)
+		output = output + c
+	
+	return output
+
