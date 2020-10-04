609ac64cb53c91179e4b245e525ef74748a1785f
Sree Revoori <srrevoori@gmail.com>
2020-10-04 09:07:24 -0400

    Update setup.py

--- cryptoLibrary/setup.py
+++ cryptoLibrary/setup.py
@@ -1,7 +1,8 @@
 from setuptools import find_packages, setup
+
 setup(
     name=’cryptoLibrary’,
-    packages=find_packages(),
+    packages=find_packages(include=[‘cryptoLibrary’]),
     version=’0.1.0',
     description=’A Python Cryptography Library’,
     author=’Me’,
