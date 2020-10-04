31899b7bd5a2fa75a242e291bfd359c77fef4a98
Sree Revoori <srrevoori@gmail.com>
2020-10-04 09:07:41 -0400

    Update setup.py

--- cryptoLibrary/setup.py
+++ cryptoLibrary/setup.py
@@ -7,4 +7,8 @@ setup(
     description=’A Python Cryptography Library’,
     author=’Me’,
     license=’MIT’,
+    install_requires=[],
+    setup_requires=[‘pytest-runner’],
+    tests_require=[‘pytest==4.4.1’],
+    test_suite=’tests’
 )
