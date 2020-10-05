f410b80703340bc1a8a02163ed1177e43d897740
Sree Revoori <srrevoori@gmail.com>
2020-10-05 07:56:42 -0400

    Update test_myfunctions.py

--- tests/test_myfunctions.py
+++ tests/test_myfunctions.py
@@ -3,4 +3,5 @@ from cryptoLibrary import cryptoTests
 from cryptoLibrary import enigma
 from cryptoLibrary import Basic_Ciphers
 from cryptoLibrary import Vigenere_Cipher
-from cryptoLibrary import password_Cracking
+from cryptoLibrary import passwordCracking
+from cryptoLibrary import Paillier_Cryptosystem
