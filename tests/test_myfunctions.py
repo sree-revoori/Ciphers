67a1726f2a38e8c02668e4165295aa79c2b680eb
Sree Revoori <srrevoori@gmail.com>
2020-10-05 07:58:17 -0400

    Update test_myfunctions.py

--- tests/test_myfunctions.py
+++ tests/test_myfunctions.py
@@ -5,3 +5,8 @@ from cryptoLibrary import Basic_Ciphers
 from cryptoLibrary import Vigenere_Cipher
 from cryptoLibrary import passwordCracking
 from cryptoLibrary import Paillier_Cryptosystem
+from cryptoLibrary import Affine_Cipher
+from cryptoLibrary import Image_Encoding
+from cryptoLibrary import ADFGVX_Cipher
+from cryptoLibrary import Index_of_Coincidence
+from cryptoLibrary import hillCipher
