e90551b2f99cd42a48609ab1a352612cb3348c49
Sree Revoori <srrevoori@gmail.com>
2020-10-07 08:40:04 -0400

    Update test_myfunctions.py

--- tests/test_myfunctions.py
+++ tests/test_myfunctions.py
@@ -19,7 +19,7 @@ def test_Affine_Cipher():
   assert Affine_Cipher.affine_encode("ANEWALPHABETWITHDIGITS12", alphabet1, 13, 29) == U2N1UIR0UANW1TW0CT3TWJZM
   assert Affine_Cipher.affine_decode("U2N1UIR0UANW1TW0CT3TWJZM", alphabet1, 13, 29) == ANEWALPHABETWITHDIGITS12
 def test_Index_of_Coincidence():
-  
+  assert Index_of_Coincidence.ioc("YTSPOQCONOVOTNOTBTDIDIQCOHDLLGOJRQCOIDBCQVOLTIYOTNJSILQCOFDQYCOIDIQCONORNDBONTQJNGDBCQLJVIQCOPQTDNPDVTPQCONODNOHOHAONDQTGGQJJVOGGXOTC").almostEquals
 def test_Basic_Ciphers():
   assert Basic_Ciphers.
 
