a9ebf8cb7e85adbe1a282fa83b0a71c50d0e219f
Sree Revoori <srrevoori@gmail.com>
2020-10-10 09:40:34 -0400

    Update test_myfunctions.py

--- tests/test_myfunctions.py
+++ tests/test_myfunctions.py
@@ -15,10 +15,7 @@ import decimal
 def almostEqual(x, y):
     return abs(x - y) < 10**-9
 def roundHalfUp(d):
-    # Round to nearest with ties going away from zero.
     rounding = decimal.ROUND_HALF_UP
-    # See other rounding options here:
-    # https://docs.python.org/3/library/decimal.html#rounding-modes
     return int(decimal.Decimal(d).to_integral_value(rounding=rounding))
 
 def test_hillCipher():
